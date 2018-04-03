e4be1d009a1ad1b568bc39f5e6eacbaa445ed831
Sanjib Barma <sanjib2barma@gmail.com>
2018-04-03 16:11:31 +0600

    Add All File

---
+++ app/src/main/java/com/example/user/spotbig/HintActivity.java
@@ -0,0 +1,30 @@
+package com.example.user.spotbig;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class HintActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView btnSkip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hint);
+
+        btnSkip = (ImageView) findViewById(R.id.btnSkip);
+        btnSkip.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnSkip){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
